fab14357e8aca5d23f7f85745d4bbabaa58266c0
Walter Angoye <angoyewally@outlook.com>
2022-09-08 00:28:01 -0700

    4-puts.c file added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include<stdio.h>
-/*
+/**
  * main - main block
- * return: 0
+ * puts - output
+ * Return: 0
  */
 int main(void)
 {
-/*
- * puts - display output
- */
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
