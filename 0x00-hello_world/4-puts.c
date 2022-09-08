3ddab04e8578482a7ab46be42fa000ec94bfcab0
Walter Angoye <angoyewally@outlook.com>
2022-09-07 23:57:44 -0700

    4-puts.c file added

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void)
+{
+/*
+ * String Iinitialisation
+ */
+	char Mystr[] = "Programming is like building a multilingual puzzle";
+/*
+ * Print String
+ */
+	puts(Mystr);
+	return (0);
+}
